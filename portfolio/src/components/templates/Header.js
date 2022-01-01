651dbe500520e074f6d1617d0e43aa78203cf9ad
yexjin <tnska0421@naver.com>
2022-01-01 19:38:45 +0900

    헤더 애니메이션 및 디자인 수정

--- portfolio/src/components/templates/Header.js
+++ portfolio/src/components/templates/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import Logo from '../atoms/Logo'
 import Buttons from '../organisms/HeaderButtons'
@@ -9,13 +9,35 @@ display: flex;
 justify-content: space-between;
 width: 100%;
 height: 79px;
-background-color: #F4F4F4;
 z-index: 5;
+box-shadow: 0;
+
+transition: box-shadow 0.5s;
+
+&.scrolled {
+  box-shadow: 0 0 15px LightGray;
+}
 `
 
 function Header() {
+
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const listener = () => {
+        setIsScrolled(window.pageYOffset > 0);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", listener);
+
+        return () => {
+        window.removeEventListener("scroll", listener);
+        };
+    }, []);
+
+
     return (
-        <Box>
+        <Box className={isScrolled ? "scrolled" : undefined}>
             <Logo />
             <Buttons />   
         </Box>
