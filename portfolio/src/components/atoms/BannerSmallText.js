494a818ce8cc1cf73415a0962de4f06ae8630c07
yexjin <tnska0421@naver.com>
2022-01-14 20:25:00 +0900

    [반응형] Banner

--- portfolio/src/components/atoms/BannerSmallText.js
+++ portfolio/src/components/atoms/BannerSmallText.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useMediaQuery } from "react-responsive";
 
 const Text = styled.div`
 width: 550px;
@@ -16,16 +17,39 @@ color: #000000;
   }
 @media screen and (max-width: 767px) {
 font-size: 15px;
+width: 294px;
+line-height: 25px;
 }
 `
 
 function BannerSmallText() {
+
+    const isPc = useMediaQuery({
+        query: "(min-width: 1280px)",
+      });
+    const isTablet = useMediaQuery({
+        query: "(min-width: 768px) and (max-width:1279px)",
+      });
+    const isMobile = useMediaQuery({ query: " (max-width: 767px)" });
+
     return (
-        <Text>
+        <>
+        {(isPc || isTablet) && (
+            <Text>
             안녕하세요, <br />
             항상 멋진 사람이 되는 것을 꿈꾸는 프론트엔드 개발자입니다. <br />
             실패를 주저하지 않고 도전하는 것을 좋아해요 :)
-        </Text>
+        </Text>)}
+
+        {(isMobile) && (
+            <Text>
+            안녕하세요, <br /> <br />
+            항상 멋진 사람이 되는 것을 꿈꾸는 프론트엔드 개발자입니다. <br /> <br />
+            실패를 주저하지 않고 도전하는 것을 좋아해요 :)
+        </Text>)}
+        
+        </>
+        
     )
 }
 
